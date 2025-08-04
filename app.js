739a88c0ea1444b8703ba5c6f57b56c2e15a2aea
Gustavo Santos <gustavos@unigranrio.br>
2025-08-04 15:22:46 -0300

    implementando função para atualizar a lista de amigos na pagina

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-let listaAmigos = [];
+let amigos = [];
 
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
     if(nome == '') {
         alert('Erro! Por favor, insira um nome.');
     } else {
-        listaAmigos.push(nome);
+        amigos.push(nome);
+        atualizarLista();
         document.getElementById('amigo').value = '';
+    }
+}
+
+function atualizarLista(){
+    let campoLista = document.getElementById('listaAmigos');
+    campoLista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++){
+        let elementoli = document.createElement('li'); //Cria o elemento
+        elementoli.textContent = amigos[i]; //Adiciona conteudo ao elemento
+        campoLista.appendChild(elementoli); //Adiciona o elemento com o conteudo no html
     }
 }
